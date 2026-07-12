fc160bb3e03d177e2a62fb6aff04246326ed2756
Sagnik Neymar <s2freestyler1234567890@gmail.com>
2026-07-13 00:53:48 +0530

    08-polar setup

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,9 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import * as schema from "@/db/schema"
+import { polarClient } from "./polar";
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
+import { Polar } from "@polar-sh/sdk"; 
 
 
 export const auth = betterAuth({
@@ -13,5 +16,26 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: true,   // automatically sign-in when someone registers
     },
-    
+
+    // ... Better Auth config
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "3851cf7d-f355-4ad2-bd15-842db9a5bde7", // ID of Product from Polar Dashboard
+                            slug: "pro" // Custom slug for easy reference in Checkout URL, e.g. /checkout/pro
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true,
+                }),
+                portal(),
+            ], 
+        }) 
+    ]
+
 });
